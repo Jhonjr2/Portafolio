bc6daec5809908a13cf1fd04a1fce7b1e22f3d52
Jhon Esteban <rangeljhon55@gmail.com>
2023-11-30 15:14:31 -0500

    change today

--- src/js/main.js
+++ src/js/main.js
@@ -1,69 +1,63 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const header = document.querySelector('header');
-    const menuItems = document.querySelectorAll('.menu a');
 
-    header.addEventListener('mouseenter', function () {
-        this.classList.add('active');
-    });
+// function menu(){
+document.addEventListener("DOMContentLoaded", () => {
+   
+    function menu() {
 
-    header.addEventListener('mouseleave', function () {
-        this.classList.remove('active');
-    });
-
-    menuItems.forEach(item => {
-        item.addEventListener('click', function () {
-            const clickedItem = this; // Almacena el elemento clicado
-
-            // Quita la clase 'selected' de todos los elementos del menú
-            menuItems.forEach(item => item.classList.remove('selected'));
-            // Agrega la clase 'selected' al elemento clicado
-            clickedItem.classList.add('selected');
-
-            // Cambia el color del texto del ícono y su texto
-            const icons = clickedItem.querySelectorAll('box-icon, ion-icon');
-            if (icons) {
-                icons.forEach(icon => {
-                    icon.style.color = '#232425';
-                });
-            }
-            const text = clickedItem.querySelector('span');
-            if (text) {
-                text.style.color = '#232425';
-            }
+        const header = document.querySelector('header');
+        const menuItems = document.querySelectorAll('.menu a');
+    
+        header.addEventListener('mouseenter', function () {
+            this.classList.add('active');
         });
-    });
-
-    // Agrego un event listener al documento para cerrar el menú cuando se haga clic fuera de él
-    document.addEventListener('click', function (event) {
-        if (!header.contains(event.target)) {
-
-            // Restaura el color del texto del ícono y su texto
-            menuItems.forEach(item => {
-                const icons = item.querySelectorAll('box-icon, ion-icon');
+    
+        header.addEventListener('mouseleave', function () {
+            this.classList.remove('active');
+        });
+    
+        menuItems.forEach(item => {
+            item.addEventListener('click', function () {
+                const clickedItem = this; // Almacena el elemento clicado
+    
+                // Quita la clase 'selected' de todos los elementos del menú
+                menuItems.forEach(item => item.classList.remove('selected'));
+                // Agrega la clase 'selected' al elemento clicado
+                clickedItem.classList.add('selected');
+    
+                // Cambia el color del texto del ícono y su texto
+                const icons = clickedItem.querySelectorAll('box-icon, ion-icon');
                 if (icons) {
                     icons.forEach(icon => {
-                        icon.style.color = ''; // Restaura el color predeterminado o al que desees
+                        icon.style.color = '#232425';
                     });
                 }
-                const text = item.querySelector('span');
+                const text = clickedItem.querySelector('span');
                 if (text) {
-                    text.style.color = ''; // Restaura el color predeterminado o al que desees
+                    text.style.color = '#232425';
                 }
             });
-        }
-    });
-
-    // Agrega un event listener a todos los enlaces del menú para el desplazamiento suave
-    menuItems.forEach(item => {
-        item.addEventListener('click', function (event) {
-            event.preventDefault(); // Evita el comportamiento predeterminado del enlace
-            const targetSectionId = this.getAttribute('href').substring(1); // Obtiene el identificador de la sección
-
-            // Desplázate suavemente a la sección correspondiente
-            document.getElementById(targetSectionId).scrollIntoView({
-                behavior: 'smooth'
-            });
         });
-    });
     
-});
+    };
+
+
+    function mode(){
+        const body = document.querySelector('body')
+        const btn = document.querySelector('.icon-mode')
+        const icon = document.querySelector('.icon-mode ion-icon')
+
+
+        btn.addEventListener('click', () => {
+            body.classList.toggle('dark')
+            
+        })
+
+
+    }
+
+    menu();
+    mode();
+
+});     
+    
+
