154737e0fdee0ed666450b95eeaea4a9db248197
Jhon Esteban <rangeljhon55@gmail.com>
2023-12-01 01:11:21 -0500

    change today

--- src/js/main.js
+++ src/js/main.js
@@ -1,77 +1,96 @@
-   
+
 function menu() {
 
     const header = document.querySelector('header');
     const menuItems = document.querySelectorAll('.menu a');
-    
-    header.addEventListener('mouseenter',function () {
+
+    header.addEventListener('mouseenter', function () {
         this.classList.add('active');
     });
-    
+
     header.addEventListener('mouseleave', function () {
         this.classList.remove('active');
     });
-    
+
     menuItems.forEach(item => {
-    item.addEventListener('click', function () {
-        const clickedItem = this; // Almacena el elemento clicado
-    
-        // Quita la clase 'selected' de todos los elementos del menú
-        menuItems.forEach(item => item.classList.remove('selected', 'focused'));
-         // Agrega la clase 'selected' al elemento clicado
-        clickedItem.classList.add('selected', 'focused');
-    
-    
-                
+        item.addEventListener('click', function () {
+            const clickedItem = this; // Almacena el elemento clicado
+
+            // Quita la clase 'selected' de todos los elementos del menú
+            menuItems.forEach(item => item.classList.remove('selected', 'focused'));
+            // Agrega la clase 'selected' al elemento clicado
+            clickedItem.classList.add('selected', 'focused');
         });
     });
-    
+
 };
 
 
-function mode(){
-        const body = document.querySelector('body')
-        const btn = document.querySelector('.icon-mode')
-        const icon = document.querySelector('.icon-mode ion-icon')
+function mode() {
+    const body = document.querySelector('body')
+    const btn = document.querySelector('.icon-mode')
+    const icon = document.querySelector('.icon-mode ion-icon')
+   
+    btn.addEventListener('click', () => {
+        body.classList.toggle('dark')
+        if (icon.name === "sunny-outline") {
+            icon.name = "moon-outline"
+        } else {
+            icon.name = "sunny-outline"
+        }
+    });
+}
+
+function sound(){
+   const btn = document.querySelector('.icon-sound')
+   const icon = document.querySelector('.icon-sound ion-icon')
+   const audio = document.querySelector('.icon-sound audio')
 
 
-        btn.addEventListener('click', () => {
-            body.classList.toggle('dark')
-            if (icon.name === "sunny-outline" ) {
-                icon.name = "moon-outline"
-                console.log(icon.name);
-            }else{
-                icon.name = "sunny-outline"
-            }
-
-            
-        })
+    btn.addEventListener('click', () => {
+      if (icon.name === 'volume-mute-outline') {
+        icon.name = 'volume-high-outline'
+      }else{
+        icon.name = 'volume-mute-outline'
+      }
+      if (audio.paused) {
+        audio.play();
+      }else{
+        audio.pause();
+      }
+    })
 
 
 }
 
-// async function getApi () {
-//     const url = 'https://fundametos-api-porfolios-dev-exsn.2.ie-1.fl0.io/api/v1/projects'
-    
-//     try {
-//         const data = await fetch(url);
-//         const res = await data.json();
-//         console.log(res);
-//         return res;
-//     }catch(error){
-//         console.log(error);
-//     }
-// }
 
 
-function main() {
-    // const projects = await getApi();
-    // console.log(projects);
+async function getApi() {
+    const url = 'https://fundametos-api-porfolios-dev-exsn.2.ie-1.fl0.io/api/v1/projects'
+
+    try {
+        const data = await fetch(url);
+        const res = await data.json();
+        console.log(res);
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function main() {
+    const projects = await getApi();
+    console.log(projects);
 
     menu();
     mode();
-
-} 
-
-   
+    sound();
     
+
+}
+
+main();
+
+
+
