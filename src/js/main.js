c57fe3924858b15527da91bc21f8d93cf5004e49
Jhon Esteban <rangeljhon55@gmail.com>
2023-12-02 12:34:57 -0500

    change today

--- src/js/main.js
+++ src/js/main.js
@@ -29,6 +29,17 @@ function menu() {
 
 };
 
+function skills() {
+   const skill = document.querySelector('.slider-skills');
+   const array = document.querySelector('.slider-skills img')
+
+   const images = Array.from(array).map(element => element.getAttribute('src'));
+   let count = 0;
+   let html = `<img src="${images[count]}" alt="hard skills">`;
+   skill.innerHTML = html;
+
+}
+
 
 function mode() {
     const body = document.querySelector('body')
@@ -81,8 +92,28 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
     // Puedes agregar marcadores, líneas, polígonos, etc. según tus necesidades.
-  }
+}
 
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("kotlin");
+       
+      
+    }
+}
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+    
+} 
 
 
 async function getApi() {
@@ -102,10 +133,11 @@ async function getApi() {
 async function main() {
     const projects = await getApi();
     console.log(projects);
-
+    skills()
     menu();
     mode();
     sound();
+    efectoHabilidades();
     
 
 }
