d2c180a8f56d06447955ac2665236f71cbf36438
Jhon Esteban <rangeljhon55@gmail.com>
2023-12-02 12:53:19 -0500

    change today

--- src/js/main.js
+++ src/js/main.js
@@ -94,26 +94,7 @@ function initMap() {
     // Puedes agregar marcadores, líneas, polígonos, etc. según tus necesidades.
 }
 
-//Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("kotlin");
-       
-      
-    }
-}
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function(){
-    efectoHabilidades();
-    
-} 
+
 
 
 async function getApi() {
