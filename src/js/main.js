4ab0701d8a8ab9a2f2aba5c0e940320714959aa8
Jhon Esteban <rangeljhon55@gmail.com>
2023-12-05 13:23:45 -0500

    lamparas solucionado el manejo de los dos eventos

--- src/js/main.js
+++ src/js/main.js
@@ -84,28 +84,44 @@ function mode() {
     const icon = document.querySelector('.icon-mode ion-icon')
     const image = document.querySelector('.dark-mode-image');
 
-    btn.addEventListener('click', () => {
+
+    const modoClick = () => {
         body.classList.toggle('dark')
-        if (icon.name === "sunny-outline") {
+        if (icon.name.includes("sunny-outline")) {
             icon.name = "moon-outline";
+            image.src = './src/img/lampara-dark.png';
+
         } else {
             icon.name = "sunny-outline";
+            image.src = './src/img/lampara-light1.png';
+
         }
         if(link.getAttribute("href")==="./src/particles/dark.css"){
             link.href= "./src/particles/brigth.css"; 
         } else {
             link.href= "./src/particles/dark.css";
         }
-    });
+    };
 
-    image.addEventListener('click', () => {
+    btn.addEventListener('click', modoClick);
+
+    const lamparaClick = () => {
         body.classList.toggle('dark')
         if (image.src.includes('lampara-light1.png')) {
             image.src = './src/img/lampara-dark.png';
+            icon.name = "moon-outline";
         } else {
             image.src = './src/img/lampara-light1.png';
+            icon.name = "sunny-outline";
         }
-    })
+        btn.removeEventListener('click', modoClick);
+        btn.addEventListener('click', modoClick);
+
+
+    }
+
+    image.addEventListener('click', lamparaClick)
+    
 }
 
 
